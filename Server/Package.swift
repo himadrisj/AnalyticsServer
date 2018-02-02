5bcb82053bddc1f8dc113b731bfb41665d2df9fe
Himadri Jyoti <himadrisj@gmail.com>
2018-02-02 18:40:01 +0530

    Skeleton server project using Perfect template

---
+++ Server/Package.swift
@@ -0,0 +1,10 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2018-02-02 13:07:03 +0000
+import PackageDescription
+let package = Package(
+	name: "HTServer",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+	]
+)
